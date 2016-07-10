86fe5f2f334315ac988b560daec3032e37ff5337
Stephen Dawes <sdawes@outlook.com>
2016-07-10 16:24:45 +0100

    set up, file config, tests, routes, controllers, views, models, databases, forms, create

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,20 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurants_new = Restaurant.new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+end
