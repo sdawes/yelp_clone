4fb42679eb74a3b530e64cd8319ea0d0169fce06
Stephen Dawes <sdawes@outlook.com>
2016-07-14 12:29:07 +0100

    refactor tests to get reviewed restaurants to pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :restaurants
-  has_many :reviews
+  has_many :restaurants, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   has_many :reviewed_restaurants, through: :reviews, source: :restaurant
 
   devise :database_authenticatable, :registerable,
